9e9278e77aed4a7f687ae58c5e497ffda0cbad2b
fannylo <75358024+fannylo@users.noreply.github.com>
2020-12-16 09:53:23 +0800

    Update check.sh

--- check_excel/check.sh
+++ check_excel/check.sh
@@ -5,7 +5,9 @@
 #cut -b 7- test2.txt > test3.txt #擷取<Name>之後的字串
 #sed 's/........$//' test3.txt > test4.csv #去除</Name>
 
-grep '<Name>' sh3-getDevice.20201203.txt | sed 's/^[ \t]*//g' | cut -b 7- | sed 's/........$//' > final.csv
+#grep '<Name>' sh3-getDevice.20201203.txt | sed 's/^[ \t]*//g' | cut -b 7- | sed 's/........$//' > final.csv
+
+grep '<Name>' sh3-getDevice.20201203.txt | sed 's/<Name>//g' | sed 's/<\/Name>//g' | sed 's/[[:space:]]//g' > final.csv
 
 #sed s/[[:space:]]//g #去除全部空格
 #sed 's/(.*)//' #去除()以及()中的字串
